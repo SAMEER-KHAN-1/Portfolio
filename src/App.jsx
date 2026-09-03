c1b093073ab288916ac26ee1b23524a900f1b63c
SAMEER-KHAN-1 <sameerkhan654kk@gmail.com>
2026-09-03 10:46:18 +0530

    adding react root, shell and stylesheet

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+export default function App() {
+
+  return (
+    <>
+      <div className="scene-stage" id="stage">
+        <div className="world" id="world">
+        </div>
+      </div>
+    </>
+  );
+}
