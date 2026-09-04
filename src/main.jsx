da12faf59bba5c2554945ee59bf5bde59803f5be
SAMEER-KHAN-1 <sameerkhan654kk@gmail.com>
2026-09-04 10:51:25 +0530

    add design tokens, wire up space.css

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/root.css";
+import "./styles/space.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
